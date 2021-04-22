c442dc09209b598382bc7563bc51544f17e9d788
Corbin Phipps <cmp99@uw.edu>
2021-04-22 10:13:04 -0500

    added file

---
+++ Week4L3.ino
@@ -0,0 +1,67 @@
+#define KEY_C 262
+#define KEY_D 294
+#define KEY_E 330
+#define KEY_F 350
+#define KEY_G 392
+
+const int INPUT_BUTTON_C_PIN = 2;
+const int INPUT_BUTTON_D_PIN = 3;
+const int INPUT_BUTTON_E_PIN = 4;
+const int INPUT_BUTTON_F_PIN = 5;
+const int INPUT_BUTTON_G_PIN = 6;
+
+const int OUTPUT_PIEZO_PIN = 9;
+const int OUTPUT_LED_PIN = LED_BUILTIN;
+
+const int DEBOUNCE_WINDOW = 40;
+
+const boolean _buttonsAreActiveLow = true;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(INPUT_BUTTON_C_PIN, INPUT_PULLUP);
+  pinMode(INPUT_BUTTON_D_PIN, INPUT_PULLUP);
+  pinMode(INPUT_BUTTON_E_PIN, INPUT_PULLUP);
+  pinMode(INPUT_BUTTON_F_PIN, INPUT_PULLUP);
+  pinMode(INPUT_BUTTON_G_PIN, INPUT_PULLUP);
+  pinMode(OUTPUT_PIEZO_PIN, OUTPUT);
+  pinMode(OUTPUT_LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  if (isButtonPressed(INPUT_BUTTON_C_PIN)) {
+    tone(OUTPUT_PIEZO_PIN, KEY_C);
+  } else if (isButtonPressed(INPUT_BUTTON_D_PIN)) {
+    tone(OUTPUT_PIEZO_PIN, KEY_D);
+  } else if (isButtonPressed(INPUT_BUTTON_E_PIN)) {
+    tone(OUTPUT_PIEZO_PIN, KEY_E);
+  } else if (isButtonPressed(INPUT_BUTTON_F_PIN)) {
+    tone(OUTPUT_PIEZO_PIN, KEY_F);
+  } else if (isButtonPressed(INPUT_BUTTON_G_PIN)) {
+    tone(OUTPUT_PIEZO_PIN, KEY_G);
+  } else {
+    noTone(OUTPUT_PIEZO_PIN);
+    digitalWrite(OUTPUT_LED_PIN, LOW);
+  }
+}
+
+boolean isButtonPressed(int btnPin) {
+  int btnVal;
+  int btnVal1 = digitalRead(btnPin);
+  delay(DEBOUNCE_WINDOW);
+  int btnVal2 = digitalRead(btnPin);
+  if (btnVal1 == btnVal2) {
+    btnVal = btnVal1;
+  }
+  if (_buttonsAreActiveLow && btnVal == LOW) {
+    digitalWrite(OUTPUT_LED_PIN, HIGH);
+    return true;
+  } else if (!_buttonsAreActiveLow && btnVal == HIGH) {
+    digitalWrite(OUTPUT_LED_PIN, HIGH);
+    return true;
+  }
+
+  return false;
+}
